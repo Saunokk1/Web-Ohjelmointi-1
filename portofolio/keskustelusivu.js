bbe2bc0b7bff71f97ba498e2a032c137132f2703
Saunokk1 <juuso.halttu@outlook.com>
2025-05-02 14:01:14 +0300

    dateblabla

--- portofolio/keskustelusivu.js
+++ portofolio/keskustelusivu.js
@@ -3,3 +3,14 @@ function avaus() {
 }
 
 avaus()
+
+function send() {
+    var date = document.getElementById('date');
+    email = document.getElementById('email');
+    comment = document.getElementById('comment');
+    user = document.getElementById('user');
+    check = document.getElementById('check');
+    menu = document.getElementById('menu');
+
+    console.log(date);
+}
