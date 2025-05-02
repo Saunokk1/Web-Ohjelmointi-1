09122adedf98c9b5f5f99b1cd53af680d59128d7
Saunokk1 <juuso.halttu@outlook.com>
2025-05-02 08:40:29 +0300

    muuttujat

---
+++ portofolio/Muuttujia ja Tietotyyppejä/index.js
@@ -0,0 +1,30 @@
+let a, b, c, d;
+
+a = 3;
+b = 5
+c = 7;
+d = 15;
+
+function summa() {
+    vastaus = a + b;
+    alert(vastaus);
+    return vastaus;
+}
+
+function vahennys() {
+    vastaus = b - c;
+    alert(vastaus);
+    return vastaus;
+}
+
+function kerto() {
+    vastaus = c * d;
+    alert(vastaus);
+    return vastaus;
+}
+
+function jako() {
+    vastaus = d / a;
+    alert(vastaus);
+    return vastaus;
+}
