81a8f1295bca9b9141f46c7b7efd1dfca5798cb8
Saunokk1 <juusohal@hotmail.com>
2025-05-09 11:32:47 +0300

    Comments better

--- portofolio/keskustelusivu.js
+++ portofolio/keskustelusivu.js
@@ -3,3 +3,19 @@ function avaus() {
 }
 
 avaus()
+
+function send(event) {
+    event.preventDefault();
+    date = document.getElementById('date').value
+    email = document.getElementById('email').value
+    type = document.getElementById('type').value
+    check = document.getElementById('check').value
+    comment = document.getElementById('comment').value
+    
+    console.log("Birthday:", date)
+    console.log("Email:", email)
+    console.log("Comment Type:", type)
+    console.log("User Agreement:", check)
+    console.log("Comment:", comment)
+    closeDialog()
+}
